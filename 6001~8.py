0659b040201eb58d640c7263186ead1a45c5350a
wkdrnsms00 <hold5732@naver.com>
2021-12-08 17:23:17 +0900

    Codeup 6024번까지 완료

---
+++ 6001~8.py
@@ -0,0 +1,5 @@
+print("'Hello'"); #'Hello'
+print('"Hello World"') # "Hello World"
+print("\"!@#$%^&*()\'"); #6006
+print("\"C:\\Download\\'hello'.py\""); #6007
+print("print(\"Hello\\nWorld\")"); #6008
