c230267c0618396d951ab1304e94d56b6ec771e3
wkdrnsms00 <hold5732@naver.com>
2021-12-09 18:32:28 +0900

    Codeup 6031번까지 완료

---
+++ 6025~31.py
@@ -0,0 +1,24 @@
+#6025
+# a, b = input().split()
+# c = int(a) + int(b)
+# print(c)
+#6026
+# a = float(input())
+# b = float(input())
+# c = a+b
+# print(c)
+#6027
+# a = int(input())
+# print('%x'%a)
+#6028
+# a = int(input())
+# print('%X'%a)
+#6029
+# a = int(input(),16)
+# print('%o'%a)
+#6030
+# n = ord(input())
+# print(n)
+#6031
+# a = chr(int(input()))
+# print(a)
