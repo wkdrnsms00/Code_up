a50ed3913a27f2e2b728f574d164b2595273959f
wkdrnsms00 <hold5732@naver.com>
2021-12-27 17:54:45 +0900

    6062 완료

---
+++ 6032~62.py
@@ -0,0 +1,107 @@
+#6032
+# n = int(input())
+# print(-n)
+#6033
+# n = ord(input())
+# print(chr(n+1))
+#6034
+# a , b = input().split()
+# c = int(a) - int(b)
+# print(c)
+#6035
+# a, b = input().split()
+# c = float(a) * float(b)
+# print(c) 
+#6036
+# word, num = input().split()
+# print(word *int(num))
+#6037
+# num = input()
+# words = input()
+# print(words *int(num))
+#6038
+# a, b = input().split(' ')
+# print(int(a)**int(b))
+#6039
+# a, b = input().split(' ')
+# print(float(a)**float(b))
+#6040
+# a, b = input().split()
+# print(int(a)//int(b))
+#6041
+# a, b = input().split()
+# print(int(a)%int(b))
+# #6042
+# a = float(input())
+# print(format(a,".2f"))
+# #6043
+# a, b = input().split()
+# c = float(a)/float(b)
+# print(format(c, ".3f"))
+#6044
+# a, b = input().split()
+# print(int(a)+int(b))
+# print(int(a)-int(b))
+# print(int(a)*int(b))
+# print(int(a)//int(b))
+# print(int(a)%int(b))
+# print(format(int(a)/int(b), ".2f"))
+#6045
+# a, b, c = input().split()
+# sum = int(a)+int(b)+int(c)
+# avg = format(sum/3, ".2f")
+# print(sum, avg )
+#6046
+# a = int(input())
+# print(a<<1)
+#6047
+# a, b = input().split()
+# print(int(a)<<int(b))
+#6048
+# a, b = input().split()
+# print(int(a)<int(b))
+#6049
+# a, b = input().split()
+# print(int(a)==int(b))
+#6050
+# a, b = input().split()
+# print(int(a)<=int(b))
+#6051
+# a, b = input().split()
+# print(int(a)!=int(b))
+#6052
+# n = int(input())
+# print(bool(n))
+#6053
+# a = bool(int(input()))
+# print(not a)
+#6054
+# a, b = input().split()
+# print(bool(int(a)) and bool(int(b)))
+#6055
+# a, b = input().split()
+# print(bool(int(a)) or bool(int(b)))
+#6056
+# a, b = input().split()
+# c = bool(int(a))
+# d = bool(int(b))
+# print((c and (not d)) or ((not c) and d))
+#6057
+# a, b = map(int, input().split())
+# print(a==b)
+#6058
+# a, b = map(int, input().split())
+# print(not(a or b))
+#6059
+# a = int(input())
+# print(~a)
+#6060
+# a, b = map(int, input().split())
+# print(a & b)
+#6061
+# a, b = map(int, input().split())
+# print(a | b)
+#6062
+a, b = map(int, input().split())
+print(a ^ b)
+
